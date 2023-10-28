bd3ebb060b8de7c8bf73280e5866af2cade14ce5
Ариди Камаль <aridi1994@mail.ru>
2023-10-28 12:26:11 +0300

    init commit

---
+++ src/main/java/ru/aridi/azon/AzonApplication.java
@@ -0,0 +1,13 @@
+package ru.aridi.azon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aridi/azon/AzonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aridi.azon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
